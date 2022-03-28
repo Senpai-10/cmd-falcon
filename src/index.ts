bdb7d330e1f4bd2005b6dbbb057202ca3c0d39fb
senpai-10 <bmjfdrh@gmail.com>
2022-03-28 14:07:36 +0300

    push command to commands

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
 import chalk from "chalk";
 
+interface I_command {
+  name: string;
+  help: string;
+  args: [
+    {
+      name: string;
+      help: string;
+      required: boolean;
+    }
+  ];
+  options: [
+    {
+      name: string;
+      help: string;
+      is_flag: boolean;
+    }
+  ];
+  callback: Function;
+}
+
 export class Cli {
   name: string;
   description: string;
+  commands: Array<I_command>;
 
   constructor(name: string, description: string) {
     this.name = name;
     this.description = description;
+    this.commands = [];
   }
 
-  public command() {
+  public command(command_props: I_command) {
+    this.commands.push(command_props);
+
     return this;
   }
 
