304f1d96bd15f6eee3889a521ca6bcf95a0b71b7
senpai-10 <bmjfdrh@gmail.com>
2022-03-28 13:32:18 +0300

    create Cli clas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-class Cli {}
+import chalk from "chalk";
+
+export class Cli {
+  description: string;
+  name: string;
+
+  constructor(name: string, description: string) {
+    this.description = description;
+    this.name = name;
+  }
+
+  public command() {
+    return this;
+  }
+
+  help() {
+    console.log(`${this.description}`);
+  }
+}
