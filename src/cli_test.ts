593d1a1f3ee203314be72466f8bff1499d0db41c
senpai-10 <bmjfdrh@gmail.com>
2022-05-11 23:21:27 +0300

    cli test

---
+++ src/cli_test.ts
@@ -0,0 +1,33 @@
+import { Cli } from "./index";
+
+interface parsed {
+    test: string;
+    verbose: string;
+    name: string;
+}
+
+const cli = new Cli("cli-test", "just a test", "0.1.0", {
+    verbose_parsing: true,
+});
+
+cli.add_option({
+    name: { long: "--test", short: "-t" },
+    is_flag: true,
+    description: "test option",
+});
+
+cli.add_option({
+    name: { long: "--verbose" },
+    is_flag: true,
+    description: "test option",
+});
+
+cli.add_option({
+    name: { long: "--name", short: "-n" },
+    is_flag: false,
+    description: "test option",
+});
+
+let parser: parsed = cli.parse();
+
+console.log(parser.name);
